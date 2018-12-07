f3facc1da231f1eda76be6349b5502a889a44683
Hugues Aubuchon <haubuchon@hotmail.com>
2018-12-07 09:48:58 -0500

    reorg app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { routing }        from './app.routing';
-import { HomeComponent } from './home';
-import { LoginComponent } from './login';
-import { AppComponent } from './app.component';
-import { NavigComponent } from './navig/navig.component';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { KendoDemoComponent } from './kendo-demo/kendo-demo.component';
 import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
 import { HttpClientModule, HTTP_INTERCEPTORS }    from '@angular/common/http';
-import { ProductGridComponent } from './product-grid/product-grid.component';
 import { GridModule } from '@progress/kendo-angular-grid';
 import { ReactiveFormsModule }    from '@angular/forms';
 import { InputsModule } from '@progress/kendo-angular-inputs';
 import { LabelModule } from '@progress/kendo-angular-label';
 import { LayoutModule } from '@progress/kendo-angular-layout';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { TreeViewModule } from '@progress/kendo-angular-treeview';
+
+import { AppComponent } from './app.component';
+import { KendoDemoComponent } from './kendo-demo/kendo-demo.component';
 import { PlannerComponent } from './planner/planner.component';
+import { ProductGridComponent } from './product-grid/product-grid.component';
 import { AxoTestComponent } from './axo-test/axo-test.component';
 import { UsersComponent } from './users/users.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { TreeViewModule } from '@progress/kendo-angular-treeview';
 import { ProjectDtlComponent } from './project-dtl/project-dtl.component';
-import { AxoProjectService } from './_services/axo-projects.service';
-import { NgxSpinnerModule } from 'ngx-spinner';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { NavigComponent } from './navig/navig.component';
 
 @NgModule({
   declarations: [
